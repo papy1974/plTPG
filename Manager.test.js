2da3a213c6e0fd63687f44a9022db3821ace108e
jessica <papylotomba@yahoo.fr>
2020-06-07 19:15:58 -0400

    test Manager.js

--- Manager.test.js
+++ Manager.test.js
@@ -6,7 +6,7 @@ test("Can set office number via constructor argument", () => {
   const e = new Manager("Foo", 1, "test@test.com", testValue);
   expect(e.officeNumber).toBe(testValue);
 });
-
+// test Manager.js
 test("getRole() should return \"Manager\"", () => {
   const testValue = "Manager";
   const e = new Manager("Foo", 1, "test@test.com", 100);
